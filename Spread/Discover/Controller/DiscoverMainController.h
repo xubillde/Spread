f16a979277da1f33e59b54e56f70dddd7dcac274
qxuewei <qxuewei@yeah.net>
2016-04-05 18:02:10 +0800

    初始化发现页

---
+++ Spread/Discover/Controller/DiscoverMainController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoverMainController.h
+//  Spread
+//
+//  Created by 邱学伟 on 16/4/5.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoverMainController : UIViewController
+
+@end
---
+++ Spread/Discover/Controller/DiscoverMainController.m
@@ -0,0 +1,81 @@
+//
+//  DiscoverMainController.m
+//  Spread
+//
+//  Created by 邱学伟 on 16/4/5.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import "DiscoverMainController.h"
+#import "MJRefresh.h"
+
+
+@interface DiscoverMainController ()
+
+//数据数组
+@property (nonatomic, strong) NSMutableArray *dataSource;
+//数据列表
+@property (nonatomic, strong) UITableView *listTable;
+
+@end
+
+@implementation DiscoverMainController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    _dataSource = [[NSMutableArray alloc] init];
+    
+    _listTable = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-64) style:UITableViewStylePlain];
+    [_listTable setDataSource:self];
+    [_listTable setDelegate:self];
+    //不显示水平滑动条
+    [_listTable setShowsHorizontalScrollIndicator:NO];
+    //不显示垂直滑动条
+    [_listTable setShowsVerticalScrollIndicator:NO];
+    //cell箭头
+    [_listTable setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    [self.view addSubview:_listTable];
+    
+    //设置刷新
+    [_listTable setMj_header:[MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(headerRereshing)]];
+    [_listTable setMj_footer:[MJRefreshAutoNormalFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreData)]];
+    // 马上进入刷新状态
+    [_listTable.header beginRefreshing];
+    
+}
+#pragma mark - 下拉刷新
+-(void)headerRereshing{
+    NSLog(@"下拉刷新");
+    [_dataSource removeAllObjects];
+    
+    //网络请求数据->
+    for (int i = 0; i<10; i++) {
+//        [_dataSource addObject:[]];
+    }
+    
+    
+}
+//上拉刷新
+-(void)loadMoreData{
+    NSLog(@"上拉刷新");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
